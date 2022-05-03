c0578a079531e01777e3a67de43bfcef5725f6a1
FGCRJS <01minted.jowly@icloud.com>
2022-05-03 15:57:07 +0900

    Fix issue - 무한루프 오류 해결

--- UnityOnlineProjectServer/Connection/Client.cs
+++ UnityOnlineProjectServer/Connection/Client.cs
@@ -7,16 +7,6 @@ namespace UnityOnlineProjectServer.Connection
 {
     internal class Client
     {
-        // Size of receive buffer.  
-        public const int BufferSize = 4096;
-
-        // Receive buffer.  
-        public byte[] buffer = new byte[BufferSize];
-
-        // Received data string.
-        public StringBuilder sb = new StringBuilder();
-
-        // Client socket.
-        public Socket workSocket = null;
+        
     }
 }
--- UnityOnlineProjectServer/Connection/Server.cs
+++ UnityOnlineProjectServer/Connection/Server.cs
@@ -11,6 +11,7 @@ namespace UnityOnlineProjectServer.Connection
         public Socket socket;
 
         private int PendingConnectionQueueCount = 100;
+        public int Port = 8080;
 
         public Server()
         {
@@ -27,41 +28,29 @@ namespace UnityOnlineProjectServer.Connection
 
             IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
             IPAddress ipAddress = ipHostInfo.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, Port);
 
-            try
-            {
-                socket.Bind(localEndPoint);
-                socket.Listen(PendingConnectionQueueCount);
-
-                while (true)
-                {
-                    socket.BeginAccept(ClientAccepted, new AsyncStateObject());
-                }
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Open Failed. Reason : " + ex.Message);
-            }
+            OpenServer(endPoint);
         }
 
         public void StartLocal()
         {
-            Console.WriteLine("Server Open");
+            Console.WriteLine("Server Start");
 
             IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
             IPAddress ipAddress = ipHostInfo.AddressList[1];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, Port);
 
+            OpenServer(endPoint);
+        }
+
+        private void OpenServer(IPEndPoint endPoint)
+        {
             try
             {
-                socket.Bind(localEndPoint);
+                socket.Bind(endPoint);
                 socket.Listen(PendingConnectionQueueCount);
-
-                while (true)
-                {
-                    socket.BeginAccept(ClientAccepted, new AsyncStateObject());
-                }
+                socket.BeginAccept(ClientAccepted, socket);
             }
             catch (Exception ex)
             {
@@ -72,7 +61,7 @@ namespace UnityOnlineProjectServer.Connection
         private void ClientAccepted(IAsyncResult ar)
         {
             // Get the socket that handles the client request.  
-            Socket clientSocket = (Socket)ar.AsyncState;
+            var clientSocket = (Socket)ar.AsyncState;
             Socket handler = clientSocket.EndAccept(ar);
 
             Console.WriteLine("Client Connected");
