645618e299311cc0c6d2c881896528643b6c2300
FGCRJS <01minted.jowly@icloud.com>
2022-05-03 09:11:16 +0900

    Initial Commit

---
+++ UnityOnlineProjectServer_/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityOnlineProjectServer_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
